795533e16f93e9bb6313b5d48adea6a2fa80b875
danil.stefaniuc <danilashtefan@gmail.com>
2021-04-18 12:28:06 +0300

    A brench commit

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,8 +77,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
-        //TODO implement feature
+        // try to fire both of the torpedo store
         if(! primaryTorpedoStore.isEmpty() && ! secondaryTorpedoStore.isEmpty()){
           firingSuccess = secondaryTorpedoStore.fire(1) && primaryTorpedoStore.fire(1);
         }
